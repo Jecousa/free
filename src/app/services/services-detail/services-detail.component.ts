9c72af68e881dfc5733fe88500197bdf0fe2a09e
jecousa <jecolousa@gmail.com>
2018-12-16 11:47:18 -0700

    updated services

--- src/app/services/services-detail/services-detail.component.ts
+++ src/app/services/services-detail/services-detail.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { Service } from '../../models/services/service';
+import { NgbModal } from '@ng-bootstrap/ng-bootstrap/modal/modal.module';
 
 @Component({
   selector: 'app-services-detail',
@@ -7,12 +8,15 @@ import { Service } from '../../models/services/service';
   styleUrls: ['./services-detail.component.css']
 })
 export class ServicesDetailComponent implements OnInit {
+  ngOnInit(): void {
+    throw new Error("Method not implemented.");
+  }
   @Input() service: Service;
 
-  constructor() { }
+  constructor(private modalService: NgbModal) {}
 
-  ngOnInit() {
+  contactModal(content) {
+    this.modalService.open(content, {backdropClass: 'light-blue-backdrop'});
   }
-  
 
 }
